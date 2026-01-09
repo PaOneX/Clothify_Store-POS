ae58216cad627e3d12accc9e077e1d247b984a70
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-01-09 16:40:00 +0530

    refactor: restore service interfaces in edu.icet

---
+++ src/main/java/edu/icet/service/CustomerService.java
@@ -0,0 +1,18 @@
+package edu.icet.service;
+
+import edu.icet.model.dto.CustomerDto;
+import edu.icet.model.dto.OrderDto;
+import javafx.collections.ObservableList;
+
+import java.util.List;
+
+public interface CustomerService {
+    int save(CustomerDto customer);
+    void update(CustomerDto customer);
+    void delete(Integer customerId);
+    ObservableList<CustomerDto> getAll();
+    ObservableList<CustomerDto> search(String term);
+    CustomerDto findById(Integer customerId);
+    CustomerDto findByPhone(String phone);
+    List<OrderDto> getPurchaseHistory(Integer customerId);
+}
