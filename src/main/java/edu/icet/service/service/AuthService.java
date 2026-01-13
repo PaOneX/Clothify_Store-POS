4b4e16bbdb5a600fe2ccb727afb99a6f3e05b7de
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-01-13 14:20:00 +0530

    refactor: restore flat service interface package

--- src/main/java/edu/icet/service/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.service;
-
-import edu.icet.model.dto.UserDto;
-
-import java.util.Optional;
-
-public interface AuthService {
-    Optional<UserDto> login(String username, String password);
-}
