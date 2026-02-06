56e20a80f15bae3389a9411dbbb08d951659827e
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-06 23:52:48 +0530

    create productdto

--- src/main/java/edu/icet/model/dto/ProductDto.java
+++ src/main/java/edu/icet/model/dto/ProductDto.java
@@ -1,4 +1,16 @@
 package edu.icet.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ProductDto {
+    private Integer id;
+    private Integer category;
+    private String description;
 }
