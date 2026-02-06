78ea43eb03627e826eacc2e6999f4e633e0c5d1c
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-02-06 23:39:16 +0530

    first commit

---
+++ src/main/java/edu/icet/controller/dashboard/DashboardController.java
@@ -0,0 +1,28 @@
+package edu.icet.controller.dashboard;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    void btnCategoryManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnIManagementnventory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProductManagement(ActionEvent event) {
+
+    }
+
+}
