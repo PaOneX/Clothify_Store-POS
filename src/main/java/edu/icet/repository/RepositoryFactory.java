263e2f4cc7dbb1a48f27b2bbe36e0251b06bcdad
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-03-03 12:43:44 +0530

    add RepositoryType enum and implement RepositoryFactory for dynamic repository creation

--- src/main/java/edu/icet/repository/RepositoryFactory.java
+++ src/main/java/edu/icet/repository/RepositoryFactory.java
@@ -1,8 +1,25 @@
 package edu.icet.repository;
 
+import edu.icet.repository.custom.Iml.ProductRepositoryImpl;
+import edu.icet.repository.custom.Iml.SupplierRepositoryImpl;
+import edu.icet.util.RepoositoryType;
+
 public class RepositoryFactory {
-    private static RepositoryFactory repositoryFactory;
+    private static RepositoryFactory instance;
 
+    private RepositoryFactory(){}
 
+    public static RepositoryFactory getInstance(){
+        return instance == null ? instance  = new RepositoryFactory() : instance;
+
+    }
+
+    public <T extends SuperRepository> T createRepositoryType(RepoositoryType type){
+        switch(type){
+            case PRODUCT: return (T) new ProductRepositoryImpl();
+            case SUPPLIER: return (T) new SupplierRepositoryImpl();
+        }
+        return null;
+    }
 
 }
