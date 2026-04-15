c37c286bb1683fc4d086e88169c58208be5c5ff9
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-04-15 11:45:00 +0530

    feat: add repository interfaces in icet core

---
+++ src/main/java/icet/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package edu.icet.repository;
+
+import edu.icet.model.dto.CustomerDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository {
+    int save(CustomerDto customer);
+    void update(CustomerDto customer);
+    void delete(Integer customerId);
+    List<CustomerDto> findAll();
+    List<CustomerDto> search(String term);
+    Optional<CustomerDto> findById(Integer customerId);
+    Optional<CustomerDto> findByPhone(String phone);
+}
