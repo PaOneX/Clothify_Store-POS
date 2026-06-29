47ef433ff6860726a145ad0bc42ac385a804dda3
Pawan Sarathchandra <sarathchandrapawan@gmail.com>
2026-06-29 10:15:00 +0530

    refactor: update desktop app entry points

--- src/main/java/edu/icet/service/AuditService.java
+++
@@ -1,6 +0,0 @@
-package edu.icet.service;
-
-public interface AuditService {
-    void log(Integer userId, String action, String entityType, Integer entityId, String details);
-    void logLogin(Integer userId, String username, boolean success);
-}
